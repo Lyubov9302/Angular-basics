2bd1224f1644ad9204f1986dbce873eb09c6732a
Lyubov9302 <lyubovyurynets@gmail.com>
2026-05-30 17:19:20 +0300

    added directives and pipe

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,49 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+export interface Role {
+  role: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  imports: [CommonModule],
 })
 export class App {
-  protected readonly title = signal('Angular-basics');
+  title: string = 'Angular basic';
+  isParagraphVisible: boolean = true;
+  list: string[] = ['List one', 'List two'];
+  roles: Role[] = [
+    {
+      role: 'admin',
+      text: 'admin',
+    },
+    {
+      role: 'user',
+      text: 'user',
+    },
+    {
+      role: 'customer',
+      text: 'Ivan',
+    },
+    {
+      role: 'user',
+      text: 'user2',
+    },
+  ];
+
+  show() {
+    console.log('isParagraphVisible', this.isParagraphVisible);
+  }
+
+  getColor(user: Role): string {
+    return user.role === 'admin' ? 'green' : user.role === 'user' ? 'red' : 'blue';
+  }
+
+  getDate() {
+    return new Date();
+  }
 }
