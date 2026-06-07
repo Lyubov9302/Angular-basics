cbfa801f88d3db28b8a62bebd270c93ab82c47bb
Lyubov9302 <lyubovyurynets@gmail.com>
2026-06-07 18:53:02 +0300

    changed app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ShowTitle } from './components/show-title/show-title';
 
 export interface Role {
   role: string;
@@ -10,40 +11,13 @@ export interface Role {
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [CommonModule],
+  imports: [CommonModule, ShowTitle],
 })
 export class App {
   title: string = 'Angular basic';
-  isParagraphVisible: boolean = true;
-  list: string[] = ['List one', 'List two'];
-  roles: Role[] = [
-    {
-      role: 'admin',
-      text: 'admin',
-    },
-    {
-      role: 'user',
-      text: 'user',
-    },
-    {
-      role: 'customer',
-      text: 'Ivan',
-    },
-    {
-      role: 'user',
-      text: 'user2',
-    },
-  ];
+  isComponentVisible: boolean = true;
 
-  show() {
-    console.log('isParagraphVisible', this.isParagraphVisible);
-  }
-
-  getColor(user: Role): string {
-    return user.role === 'admin' ? 'green' : user.role === 'user' ? 'red' : 'blue';
-  }
-
-  getDate() {
-    return new Date();
+  changeTitle(newTitle: string) {
+    this.title = newTitle;
   }
 }
